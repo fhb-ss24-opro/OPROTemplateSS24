f589120942c6bfee9071da7ecfa000859bc68230
BarbaraSachLackner <78683974+BarbaraSachLackner@users.noreply.github.com>
2023-06-10 13:18:49 +0200

    Update Main.java
    removed code

--- src/main/java/org/lecture/Main.java
+++ src/main/java/org/lecture/Main.java
@@ -8,41 +8,8 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        FurnitureOrderService furnitureOrderService = new FurnitureOrderService();
-        // furnitureOrderService.orderFurniture();
-
-        zonedDateTime();
-        //localDateTime();
-
-
+      //TODO write code
     }
 
-    private static void zonedDateTime() {
-        ZonedDateTime zdt = ZonedDateTime.now();
-        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
-        System.out.println(dtf.format(zdt));
-        dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
-        System.out.println(dtf.format(zdt));
-        dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
-        System.out.println(dtf.format(zdt));
-        dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL);
-        System.out.println(dtf.format(zdt));
-    }
-
-    private static void localDateTime() {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        localDateTime
-                .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-                        .withLocale(Locale.UK));
-        System.out.println(localDateTime);
-        localDateTime
-                .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
-                        .withLocale(Locale.UK));
-        System.out.println(localDateTime);
-
-        localDateTime
-                .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
-                        .withLocale(Locale.UK));
-        System.out.println(localDateTime);
-    }
+   
 }
