96b009e7e4f505658b460c23974d9326d7898ce8
BarbaraSachLackner <78683974+BarbaraSachLackner@users.noreply.github.com>
2023-06-10 13:19:24 +0200

    Update Main.java
    removed imports

--- src/main/java/org/lecture/Main.java
+++ src/main/java/org/lecture/Main.java
@@ -1,11 +1,5 @@
 package org.lecture;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
       //TODO write code
