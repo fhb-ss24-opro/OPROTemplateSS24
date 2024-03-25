375a3bde0d39ac433c09b89c79b5691fa6894b9e
BarbaraSachLackner <barbara-edith.sach-lackner@fh-burgenland.at>
2024-03-25 11:33:45 +0100

    added log4j2 config added log to gitignore

--- src/main/java/org/lecture/Main.java
+++ src/main/java/org/lecture/Main.java
@@ -1,9 +1,13 @@
 package org.lecture;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Main {
+
     public static void main(String[] args) {
-      //TODO write code
+        log.atInfo().log("Hello World");
     }
 
-   
+
 }
