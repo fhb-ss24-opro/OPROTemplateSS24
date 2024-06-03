10e443501211637b73ec3dde6e553e12a61fa9f0
BarbaraSachLackner <barbara-edith.sach-lackner@fh-burgenland.at>
2024-06-03 09:47:00 +0200

    Revert "priveded guidance for testing input"
    This reverts commit 3a8fbfa66964943efba689c246425fd17da781e6.

--- src/test/java/org/lecture/GameTests.java
+++ src/test/java/org/lecture/GameTests.java
@@ -1,13 +1,8 @@
 package org.lecture;
 
-import org.lecture.Characters.Player;
+import Characters.Player;
 import org.junit.jupiter.api.Test;
-import org.lecture.rooms.MagicRoom;
-import org.lecture.rooms.Room;
-import org.lecture.rooms.RoomFactory;
-import org.lecture.rooms.TrapRoom;
-
-import java.io.ByteArrayInputStream;
+import Rooms.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,17 +19,15 @@ class GameTests {
     void testPlayerSurvivesMagicRoom() {
         Player player = new Player(2, "Human", "Warlock", 14, "Lucifer");
         Room magicRoom = new MagicRoom();
-        provideInput("Rock");
         assertTrue(magicRoom.surviveRoom(player), "Player should survive the magic room.");
     }
 
 
-   //too difficult to test! because you you'd have to parse the substraction from the console
+    @Test
     void testPlayerSurvivesTrapRoomWithCorrectAnswer() {
         Player player = new Player(1, "Elf", "Archer", 10, "Legolas");
         Room trapRoom = new TrapRoom();
         // Simulate correct answer in quest
-
         assertTrue(trapRoom.surviveRoom(player), "Player should survive the trap room with a correct answer.");
     }
 
@@ -43,10 +36,5 @@ class GameTests {
         RoomFactory factory = new RoomFactory();
         Room room = factory.createRoom(2, 2);
         assertNotNull(room, "Room should be created by factory.");
-    }
-
-    void provideInput(String data) {
-        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
-        System.setIn(testIn);
     }
 }
