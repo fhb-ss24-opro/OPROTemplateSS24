46b6aa5ce0d3914ce0cdefafabe385bca7f149bf
BarbaraSachLackner <barbara-edith.sach-lackner@fh-burgenland.at>
2024-06-03 09:46:46 +0200

    Revert "made method private"
    This reverts commit ec4c12268fb618fb3c657e09f72cc97a7875957e.

--- src/test/java/org/lecture/GameTests.java
+++ src/test/java/org/lecture/GameTests.java
@@ -45,7 +45,7 @@ class GameTests {
         assertNotNull(room, "Room should be created by factory.");
     }
 
-    private void provideInput(String data) {
+    void provideInput(String data) {
         ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
         System.setIn(testIn);
     }
